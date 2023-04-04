061e7cbcef45db9676db9b46780340196f00aa56
Anne LOUIS <anne.louis51100@gmail.com>
2023-04-04 18:49:35 +0200

    Repat a component with map

--- src/App.jsx
+++ src/App.jsx
@@ -2,49 +2,53 @@ import { useState } from 'react'
 import './App.css'
 import PokemonCard from './components/PokemonCard'
 import NavBar from './components/NavBar'
-import PropTypes from "prop-types"
 
+
+
+
+const pokemonList = [
+  {
+      name: "bulbasaur",
+      imgSrc:
+        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
+    },
+    {
+      name: "charmander",
+      imgSrc:
+        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/4.png",
+    },
+    {
+      name: "squirtle",
+      imgSrc:
+        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/7.png",
+    },
+    {
+      name: "pikachu",
+      imgSrc:
+        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/25.png",
+    },
+    {
+      name: "mew",
+    },
+  ];
 
 function App() {
 
-   const pokemonList = [
-    {
-        name: "bulbasaur",
-        imgSrc:
-          "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
-      },
-      {
-        name: "charmander",
-        imgSrc:
-          "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/4.png",
-      },
-      {
-        name: "squirtle",
-        imgSrc:
-          "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/7.png",
-      },
-      {
-        name: "pikachu",
-        imgSrc:
-          "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/25.png",
-      },
-      {
-        name: "mew",
-      },
-    ];
-    // 
+   
+    
+    
     // States
     let [pokemonIndex, setPokemonIndex]=useState(0);
 
 
     return (
-        <><div>
+        <div>
           <PokemonCard pokemon={pokemonList[pokemonIndex]}/>
-          <NavBar pokemons={pokemonList.length} pokemonIndex={pokemonIndex} setPokemonIndex={setPokemonIndex} />
+          <NavBar pokemonList={pokemonList} pokemonIndex={pokemonIndex} setPokemonIndex={setPokemonIndex} />
         </div>
 
-        </>
-  );
+     
+    );
 }
 
 export default App
--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,24 +1,17 @@
-import PropTypes from "prop-types"
-import PokemonCard from "./PokemonCard";
 
 
-const NavBar =({pokemons, pokemonIndex, setPokemonIndex}) =>
+
+const NavBar =({pokemonList, pokemonIndex, setPokemonIndex}) =>
 {
-    const previousPokemonIndex =() => setPokemonIndex(pokemonIndex - 1);
-    const nextPokemonIndex =() => setPokemonIndex(pokemonIndex + 1);
-    
-
     
 
 
     return (
-        <><div>
-          {/* <PokemonCard pokemon={pokemonList[pokemonIndex]}/> */}
-          {pokemonIndex>0 ? <button onClick={previousPokemonIndex}> Previous </button> : null}
-          {pokemonIndex < pokemons -1 ? <button onClick={nextPokemonIndex}> Next </button> : null}
-        </div>
+        <div>
+                   {pokemonList.map((pokemon, index) => (<button type="button" onClick={() => setPokemonIndex(index)} key={index}> {pokemon.name} </button>))}
 
-        </>
+        </div>
+     
   );
 }
 
